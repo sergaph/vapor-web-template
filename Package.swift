5a467b3c307d87f6044ec4b04cc51fa5621aff2d
sergey.maslennikov <sergey.maslennikov@aliexpress.ru>
2023-10-14 21:45:25 +0300

    Add leaf templates with bootstrap

--- Package.swift
+++ Package.swift
@@ -1,28 +1,30 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
-    name: "VaporWebSample",
+    name: "vapor-web-template",
     platforms: [
-       .macOS(.v13)
+       .macOS(.v12)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "App",
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "Leaf", package: "leaf"),
             ]
         ),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
-
-            // Workaround for https://github.com/apple/swift-package-manager/issues/6940
-            .product(name: "Vapor", package: "vapor"),
-        ])
+//        .testTarget(name: "AppTests", dependencies: [
+//            .target(name: "App"),
+//            .product(name: "XCTVapor", package: "vapor"),
+//
+//            // Workaround for https://github.com/apple/swift-package-manager/issues/6940
+//            .product(name: "Vapor", package: "vapor"),
+//        ])
     ]
 )
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,24 @@
 import Vapor
 
-func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
+struct WelcomeContext: Encodable {
+    var title: String
+    var numbers: [Int]
+}
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+func routes(_ app: Application) throws {
+//    app.get { req async in
+//        "It works!"
+//    }
+
+    app.get { req async throws -> View in
+//        return try await req.view.render("home", ["name": "masle.net"])
+        return try await req.view.render("index", ["title": "masle.net"])
+    }
+    
+    
+    app.get("hello") { req async throws -> View in
+//        "Hello, world!"
+//        return try await req.view.render("hello", ["name": "Leaf"])
+        return try await req.view.render("hello", WelcomeContext(title: "Hello!", numbers: [42, 9001]))
     }
 }
